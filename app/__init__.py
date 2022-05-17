28a96ea47790b74be84be9ba66b66f5d3eeaa66a
calebkabaya <calebkabaya@gmail.com>
2022-05-17 14:00:04 +0300

    added configuration for auth folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,21 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 from config import config_options
 
 bootstrap = Bootstrap()
-# db = SQLAlchemy()
-# from flask_uploads import UploadSet,configure_uploads,IMAGES
-# from flask_login import LoginManager
+db = SQLAlchemy()
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_login import LoginManager
 
-# login_manager = LoginManager()
-# login_manager.session_protection = 'strong'
-# login_manager.login_view = 'auth.login'
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
-# photos = UploadSet('photos',IMAGES)
+photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
 
     app = Flask(__name__)
@@ -26,15 +26,15 @@ def create_app(config_name):
 
     # Initializing flask extensions
     bootstrap.init_app(app)
-    # db.init_app(app)
-    # login_manager.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
 
     # # configure UploadSet
-    # configure_uploads(app,photos)
+    configure_uploads(app,photos)
     
     # # Registering the blueprint
-    # from .auth import auth as auth_blueprint
-    # app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
     
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
